95b053fa3cddde258b3787e2ae7aa220a599a9b8
Kartik Reddy <kartiklaw@gmail.com>
2018-03-08 23:35:25 -0500

    Chagne class name to Constants.

--- sample/src/main/java/pajkouisn/utilities/constants/Constants.java
+++ sample/src/main/java/pajkouisn/utilities/constants/Constants.java
@@ -1,13 +1,13 @@
 package pajkouisn.utilities.constants;
 
-public class Constant 
+public class Constants
 {
 	/**
 	 * The caller references the constants using <tt>Consts.EMPTY_STRING</tt>, 
 	 * and so on. Thus, the caller should be prevented from constructing objects of 
 	 * this class, by declaring this private constructor. 
 	 */
-	private Constant()
+	private Constants()
 	{
 		//	this prevents even the native class from 
 		//	calling this ctor as well :
